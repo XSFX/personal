0627c7e81321a3e9bb5057458d0ba1f9dcdc3966
gblagov <g.blagov@gmail.com>
2018-06-08 07:13:39 +0300

    Fix opening file issue for windows.

--- lib/config.py
+++ lib/config.py
@@ -1,11 +1,13 @@
+import os
 import json
-class Config:
-	def __init__(self, service):
-		content = None
-		with open('./access.cfg', 'r') as f:
-			content = json.loads(f.read())
-		self.cfg = content[service]
 
+
+class Config:
+    def __init__(self, service):
+        content = None
+        with open(os.path.join(os.path.dirname(__file__), "../access.cfg"), 'r') as f:
+            content = json.loads(f.read())
+        self.cfg = content[service]
 
 
 d = Config("database").cfg
